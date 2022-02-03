ae9f0007a79997bb48c6e6e9d06911b80e421e3d
TinsellyCone <60909484+TinsellyCone@users.noreply.github.com>
2022-02-02 21:35:14 -0500

    updated markdown descriptions

--- rockpaperscissors/script.js
+++ rockpaperscissors/script.js
@@ -4,12 +4,15 @@ let random = getRandomNum(0,3);
 function buttonpress (buttonnum) {
     if(buttonnum == 3) {
         button = "scissors";
+        location.reload();
     }
     else if(buttonnum == 2) {
         button = "paper";
+        location.reload();
     }
     else if(buttonnum == 1) {
         button = "rock";
+        location.reload();
     }
     if (random == buttonnum) {
         alert("You Win!");
--- tictactoe/tictactoe.md
+++ tictactoe/tictactoe.md
@@ -1,4 +1,4 @@
-TicTacToe
+# TicTacToe
 
 TicTacToe is a classic game in which two players play as either an X or an O.
 The object of the game is to get three Xs or Os in a line.
